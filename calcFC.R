0891abda7bf6bc7448e25c062f1fd4385068e354
vdutell <vashadutell@gmail.com>
2014-11-17 17:02:43 -0600

    Added Fold Change Calc Functions

---
+++ calcFC.R
@@ -0,0 +1,26 @@
+##vasha's beautiful fold change function with help from Dar!!!
+calc.fc <- function(va,vb){  
+    va <- va+.1
+    vb <- vb+.1
+    fc.pos <- vb/va
+    fc.neg <- va/vb
+    ##make TRUE/FALSE vectors for lessthan/greaterthan
+    pos.v <- fc.pos*(fc.pos >= fc.neg)
+    neg.v <- fc.neg*(fc.neg > fc.pos)
+    ##return the value that was TRUE
+    return(as.numeric(pos.v - neg.v))
+}
+
+#log fold change calculation function
+calc.log.fc <- function(va,vb){  
+    va <- va+.1
+    vb <- vb+.1
+    fc.pos <- vb/va
+    fc.neg <- va/vb
+    ##make TRUE/FALSE vectors for lessthan/greaterthan
+    pos.v <- log2(fc.pos)*(fc.pos >= fc.neg)
+    neg.v <- log2(fc.neg)*(fc.neg > fc.pos)
+    ##return the value that was TRUE
+    return(as.numeric(pos.v - neg.v))
+}  
+
